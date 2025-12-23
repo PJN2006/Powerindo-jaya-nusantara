3b6ccc87d6680840f8e2d56f1581561fb25dba9c
MasIcad <irsyadannafi2006@gmail.com>
2025-12-24 01:05:33 +0700

    update2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "@/app/globals.css";
 import Navbar from "@/components/layout/Navbar";
-import SmoothScroll from "@/components/layout/SmoothScroll"; // Import wrapper baru
-import Footer from "@/components/layout/Footer";
+import SmoothScroll from "@/components/layout/SmoothScroll";
+import Footer from "@/components/layout/Footer"; // Pastikan baris ini ada
 
 const jakarta = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
   variable: "--font-plus-jakarta-sans", 
 });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <body className={`${jakarta.variable} ${jakarta.className} antialiased`}>
         <SmoothScroll>
           <Navbar />
-          {children}
-          <Footer /> {/* Tambahkan ini di sini */}
+          <main>{children}</main>
+          <Footer />
         </SmoothScroll>
       </body>
     </html>
